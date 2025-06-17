cc98fbeb9d768aeb47a55c80fce6cfc1cb5b25d1
deepakmauryacs <62381959+deepakmauryacs@users.noreply.github.com>
2025-06-17 16:29:27 +0530

    Fix select2 scripts commented in admin product views

--- resources/views/admin/products/approved.blade.php
+++ resources/views/admin/products/approved.blade.php
@@ -72,14 +72,8 @@
             </div>
         </div>
     </div>
-    <!-- Add before your <script>
-        block or inside your layout-- >
-        <
-        link href = "https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css"
-        rel = "stylesheet" / >
-            <
-            script src = "https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js" >
-    </script>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script>
         $(document).ready(function() {
             // Initial fetch of data when the page loads
