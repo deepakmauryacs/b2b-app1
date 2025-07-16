798de78ce4240e0d65a27570def5003038bbcb0b
deepakmauryacs <62381959+deepakmauryacs@users.noreply.github.com>
2025-07-16 23:36:49 +0530

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,19 @@ use App\Http\Controllers\ActivityLogController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\VendorController;
 use App\Http\Controllers\Admin\VendorExportController;
 use App\Http\Controllers\Admin\BuyerController;
 use App\Http\Controllers\Admin\PendingProductController;
 use App\Http\Controllers\Admin\ApprovedProductController;
-use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\RejectedProductController;
-use App\Http\Controllers\Admin\UserController;
+
 use App\Http\Controllers\Vendor\VendorDashboardController;
 use App\Http\Controllers\Vendor\VendorProfileController;
 use App\Http\Controllers\Vendor\VendorProductController;
-use App\Http\Controllers\Vendor\VendorSubscriptionController;
 use App\Http\Controllers\Vendor\VendorPasswordController;
 use App\Http\Controllers\Buyer\HomeController;
 use App\Http\Controllers\Buyer\BuyRequirementController;
@@ -51,50 +51,6 @@ Route::middleware(['auth'])->group(function () {
 
     Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
 
-    Route::prefix('admin/categories')->name('admin.categories.')->group(function () {
-        Route::get('list', [CategoryController::class, 'index'])->name('index');
-        Route::get('data', [CategoryController::class, 'getCategories'])->name('data');
-        Route::get('create', [CategoryController::class, 'create'])->name('create');
-        Route::post('store', [CategoryController::class, 'store'])->name('store');
-        Route::get('edit/{id}', [CategoryController::class, 'edit'])->name('edit');
-        Route::put('update/{id}', [CategoryController::class, 'update'])->name('update');
-        Route::delete('delete/{id}', [CategoryController::class, 'destroy'])->name('destroy');
-        Route::get('get-subcategories/{parentId}', [CategoryController::class, 'getSubcategories'])->name('get-subcategories');
-    });
-
-    Route::prefix('admin/banners')->name('admin.banners.')->group(function () {
-        Route::get('list', [App\Http\Controllers\Admin\BannerController::class, 'index'])->name('index');
-        Route::get('data', [App\Http\Controllers\Admin\BannerController::class, 'getBanners'])->name('data');
-        Route::get('render-table', [App\Http\Controllers\Admin\BannerController::class, 'renderBannersTable'])->name('render-table');
-        Route::get('create', [App\Http\Controllers\Admin\BannerController::class, 'create'])->name('create');
-        Route::post('store', [App\Http\Controllers\Admin\BannerController::class, 'store'])->name('store');
-        Route::get('edit/{id}', [App\Http\Controllers\Admin\BannerController::class, 'edit'])->name('edit');
-        Route::put('update/{id}', [App\Http\Controllers\Admin\BannerController::class, 'update'])->name('update');
-    });
-
-    Route::prefix('admin/vendors')->name('admin.vendors.')->group(function () {
-        Route::get('list', [VendorController::class, 'index'])->name('index');
-        Route::get('data', [VendorController::class, 'getVendors'])->name('data');
-        Route::get('create', [VendorController::class, 'create'])->name('create');
-        Route::post('store', [VendorController::class, 'store'])->name('store');
-        Route::get('edit/{id}', [VendorController::class, 'edit'])->name('edit');
-        Route::put('update/{id}', [VendorController::class, 'update'])->name('update');
-        Route::post('update-profile-verification', [VendorController::class, 'updateProfileVerification'])->name('update-profile-verification');
-        Route::get('{id}', [VendorController::class, 'show'])->name('show');
-        Route::get('search', [VendorController::class, 'search'])->name('search');
-        Route::get('fetch', [VendorController::class, 'fetchVendors'])->name('fetch');
-        Route::get('render-table', [VendorController::class, 'renderVendorsTable'])->name('render-table');
-        Route::get('export-data', [VendorController::class, 'exportData'])->name('export-data');
-    });
-
-    Route::prefix('admin/vendor-exports')->name('admin.vendor-exports.')->group(function () {
-        Route::get('/', [VendorExportController::class, 'index'])->name('index');
-        Route::get('create', [VendorExportController::class, 'create'])->name('create');
-        Route::post('store', [VendorExportController::class, 'store'])->name('store');
-        Route::get('{id}/download', [VendorExportController::class, 'download'])->name('download');
-        Route::delete('{id}', [VendorExportController::class, 'destroy'])->name('destroy');
-    });
-
     Route::prefix('admin/vendor-subscriptions')->name('admin.vendor-subscriptions.')->group(function () {
         Route::get('/', [App\Http\Controllers\Admin\VendorSubscriptionController::class, 'index'])->name('index');
         Route::get('render-table', [App\Http\Controllers\Admin\VendorSubscriptionController::class, 'renderSubscriptionsTable'])->name('render-table');
@@ -117,6 +73,7 @@ Route::middleware(['auth'])->group(function () {
         Route::get('{id}/print', [App\Http\Controllers\Admin\BuyerSubscriptionController::class, 'show'])->name('print');
     });
 
+      
     Route::prefix('admin/plans')->name('admin.plans.')->group(function () {
         Route::get('/', [App\Http\Controllers\Admin\PlanController::class, 'index'])->name('index');
         Route::get('create', [App\Http\Controllers\Admin\PlanController::class, 'create'])->name('create');
@@ -144,19 +101,55 @@ Route::middleware(['auth'])->group(function () {
         Route::put('update/{id}', [UserController::class, 'update'])->name('update');
     });
 
+    Route::prefix('admin/categories')->name('admin.categories.')->group(function () {
+        Route::get('list', [CategoryController::class, 'index'])->name('index');
+        Route::get('data', [CategoryController::class, 'getCategories'])->name('data');
+        Route::get('create', [CategoryController::class, 'create'])->name('create');
+        Route::post('store', [CategoryController::class, 'store'])->name('store');
+        Route::get('edit/{id}', [CategoryController::class, 'edit'])->name('edit');
+        Route::put('update/{id}', [CategoryController::class, 'update'])->name('update');
+        Route::delete('delete/{id}', [CategoryController::class, 'destroy'])->name('destroy');
+        Route::get('get-subcategories/{parentId}', [CategoryController::class, 'getSubcategories'])->name('get-subcategories');
+    });
+
+    Route::prefix('admin/vendors')->name('admin.vendors.')->group(function () {
+        Route::get('list', [VendorController::class, 'index'])->name('index');
+        Route::get('data', [VendorController::class, 'getVendors'])->name('data');
+        Route::get('create', [VendorController::class, 'create'])->name('create');
+        Route::post('store', [VendorController::class, 'store'])->name('store');
+        Route::get('edit/{id}', [VendorController::class, 'edit'])->name('edit');
+        Route::put('update/{id}', [VendorController::class, 'update'])->name('update');
+        Route::post('update-profile-verification', [VendorController::class, 'updateProfileVerification'])->name('update-profile-verification');
+        Route::get('{id}', [VendorController::class, 'show'])->name('show');
+        Route::get('search', [VendorController::class, 'search'])->name('search');
+        Route::get('fetch', [VendorController::class, 'fetchVendors'])->name('fetch');  
+    });
+
+    Route::get('admin/vendor-search', [VendorController::class, 'search'])->name('admin.vendors.search');
+
+    Route::get('vendors/render-table', [VendorController::class, 'renderVendorsTable'])->name('admin.vendors.render-table');
+    Route::get('export-data', [VendorController::class, 'exportData'])->name('admin.vendors.export-data');
+
+    Route::prefix('admin/vendor-exports')->name('admin.vendor-exports.')->group(function () {
+        Route::get('/', [VendorExportController::class, 'index'])->name('index');
+        Route::get('create', [VendorExportController::class, 'create'])->name('create');
+        Route::post('store', [VendorExportController::class, 'store'])->name('store');
+        Route::get('{id}/download', [VendorExportController::class, 'download'])->name('download');
+        Route::delete('{id}', [VendorExportController::class, 'destroy'])->name('destroy');
+    });
+
     Route::prefix('admin/buyers')->name('admin.buyers.')->group(function () {
         Route::get('list', [BuyerController::class, 'index'])->name('index');
-        Route::get('data', [BuyerController::class, 'getBuyers'])->name('data');
-        Route::get('render-table', [BuyerController::class, 'renderBuyersTable'])->name('render-table');
         Route::get('search', [BuyerController::class, 'search'])->name('search');
         Route::get('create', [BuyerController::class, 'create'])->name('create');
         Route::post('store', [BuyerController::class, 'store'])->name('store');
         Route::get('edit/{id}', [BuyerController::class, 'edit'])->name('edit');
         Route::put('update/{id}', [BuyerController::class, 'update'])->name('update');
-        Route::post('update-profile-verification', [BuyerController::class, 'updateProfileVerification'])->name('update-profile-verification');
         Route::get('{id}', [BuyerController::class, 'show'])->name('show');
-        Route::delete('delete/{id}', [BuyerController::class, 'destroy'])->name('delete');
+        Route::post('update-profile-verification', [BuyerController::class, 'updateProfileVerification'])->name('update-profile-verification');
+
     });
+    Route::get('buyers/render-table', [BuyerController::class, 'renderBuyersTable'])->name('admin.buyers.render-table');
 
     Route::prefix('admin/products')->group(function () {
         Route::get('/pending', [PendingProductController::class, 'index'])->name('admin.products.pending');
@@ -175,12 +168,25 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/rejected/data', [RejectedProductController::class, 'getRejectedProducts'])->name('admin.products.rejected.data');
         Route::get('rejected/render-table', [RejectedProductController::class, 'renderRejectedProductsTable'])->name('admin.rejected.products.render-table');
         Route::get('/rejected/{id}', [RejectedProductController::class, 'show'])->name('admin.products.rejected.show');
-    Route::post('/rejected/{id}/restore', [RejectedProductController::class, 'restore'])->name('admin.products.rejected.restore');
+        Route::post('/rejected/{id}/restore', [RejectedProductController::class, 'restore'])->name('admin.products.rejected.restore');
     });
+
+
+     Route::prefix('admin/banners')->name('admin.banners.')->group(function () {
+        Route::get('list', [App\Http\Controllers\Admin\BannerController::class, 'index'])->name('index');
+        Route::get('data', [App\Http\Controllers\Admin\BannerController::class, 'getBanners'])->name('data');
+        Route::get('render-table', [App\Http\Controllers\Admin\BannerController::class, 'renderBannersTable'])->name('render-table');
+        Route::get('create', [App\Http\Controllers\Admin\BannerController::class, 'create'])->name('create');
+        Route::post('store', [App\Http\Controllers\Admin\BannerController::class, 'store'])->name('store');
+        Route::get('edit/{id}', [App\Http\Controllers\Admin\BannerController::class, 'edit'])->name('edit');
+        Route::put('update/{id}', [App\Http\Controllers\Admin\BannerController::class, 'update'])->name('update');
+    });
+
 
     Route::get('/vendor/dashboard', [VendorDashboardController::class, 'index'])->name('vendor.dashboard');
     Route::get('/vendor/profile', [VendorProfileController::class, 'profile'])->name('vendor.profile.show');
     Route::post('profile/update', [VendorProfileController::class, 'update'])->name('vendor.profile.update');
+    
     Route::get('/vendor/change-password', [\App\Http\Controllers\Vendor\VendorPasswordController::class, 'edit'])->name('vendor.password.edit');
     Route::post('/vendor/change-password', [\App\Http\Controllers\Vendor\VendorPasswordController::class, 'update'])->name('vendor.password.update');
 
@@ -188,7 +194,7 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/vendor/subscription', [VendorSubscriptionController::class, 'store'])->name('vendor.subscription.store');
     Route::get('/vendor/subscription/invoice', [VendorSubscriptionController::class, 'invoice'])->name('vendor.subscription.invoice');
 
-    Route::prefix('vendor/products')->name('vendor.products.')->group(function () {
+   Route::prefix('vendor/products')->name('vendor.products.')->group(function () {
         Route::get('list', [VendorProductController::class, 'index'])->name('index');
         Route::get('approved', [VendorProductController::class, 'approved'])->name('approved');
         Route::get('pending', [VendorProductController::class, 'pending'])->name('pending');
@@ -196,15 +202,16 @@ Route::middleware(['auth'])->group(function () {
         Route::get('data', [VendorProductController::class, 'getProducts'])->name('data');
         Route::get('render-table', [VendorProductController::class, 'renderProductsTable'])->name('render-table');
         Route::get('create', [VendorProductController::class, 'create'])->name('create');
-        Route::get('get-subcategories/{parentId}', [VendorProductController::class, 'getSubcategories'])->name('get-subcategories');
         Route::post('save', [VendorProductController::class, 'store'])->name('store');
         Route::get('{id}/edit', [VendorProductController::class, 'edit'])->name('edit');
         Route::put('update/{id}', [VendorProductController::class, 'update'])->name('update');
         Route::get('{id}', [VendorProductController::class, 'show'])->name('show');
         Route::delete('delete/{id}', [VendorProductController::class, 'destroy'])->name('destroy');
-        Route::get('export-data', [VendorProductController::class, 'exportData'])->name('export-data');
+        Route::get('export-data/list', [VendorProductController::class, 'exportData'])->name('export-data');
         Route::get('export/init', [VendorProductController::class, 'exportInit'])->name('export.init');
     });
+
+     Route::get('vendor/get-subcategories/{parentId}', [VendorProductController::class, 'getSubcategories'])->name('vendor.get-subcategories');
 
     Route::prefix('vendor/help-support')->name('vendor.help-support.')->group(function () {
         Route::get('list', [App\Http\Controllers\Vendor\VendorHelpSupportController::class, 'index'])->name('index');
@@ -224,19 +231,15 @@ Route::middleware(['auth'])->group(function () {
         Route::get('export/chunk', [App\Http\Controllers\Vendor\VendorInventoryController::class, 'exportChunk'])->name('export.chunk');
     });
 
-    Route::prefix('vendor/warehouses')->name('vendor.warehouses.')->group(function () {
+     Route::prefix('vendor/warehouses')->name('vendor.warehouses.')->group(function () {
         Route::get('list', [App\Http\Controllers\Vendor\WarehouseController::class, 'index'])->name('index');
         Route::get('render-table', [App\Http\Controllers\Vendor\WarehouseController::class, 'renderTable'])->name('render-table');
         Route::post('store', [App\Http\Controllers\Vendor\WarehouseController::class, 'store'])->name('store');
         Route::put('update/{id}', [App\Http\Controllers\Vendor\WarehouseController::class, 'update'])->name('update');
         Route::delete('delete/{id}', [App\Http\Controllers\Vendor\WarehouseController::class, 'destroy'])->name('delete');
     });
+
 });
-
-Route::get('/buyer/dashboard', function () {
-    // buyer dashboard view or controller
-})->name('buyer.dashboard');
-
 
 Route::get('/buyer', [HomeController::class, 'index'])->name('buyer.index');
 Route::get('/buyer/category/{slug}', [HomeController::class, 'subCategoryPage'])->name('buyer.sub-category-page');
@@ -246,11 +249,18 @@ Route::get('/buyer/sub-categories/{category}', [HomeController::class, 'subCateg
 Route::get('/buyer/products/{subCategory}', [HomeController::class, 'productsBySubCategory'])->name('buyer.subcategory-products');
 Route::get('/buyer/top-products', [HomeController::class, 'topProducts'])->name('buyer.top-products');
 Route::get('/buyer/search-suggestions', [HomeController::class, 'searchSuggestions'])->name('buyer.search-suggestions');
-Route::get('/buyer/post-buy', [BuyRequirementController::class, 'create'])->name('buyer.post-buy.create');
+Route::get('/buyer/get-best-deal', [BuyRequirementController::class, 'create'])->name('buyer.get-best-deal.create');
 Route::post('/buyer/post-buy', [BuyRequirementController::class, 'store'])->name('buyer.post-buy.store');
 
 // Product list with load more
 Route::get('/buyer/products-list', [\App\Http\Controllers\Buyer\ProductController::class, 'index'])->name('buyer.products.list-page');
 Route::get('/buyer/products-list/data', [\App\Http\Controllers\Buyer\ProductController::class, 'list'])->name('buyer.products.list');
 
+
+
 Route::post('/newsletter/subscribe', [\App\Http\Controllers\NewsletterController::class, 'store'])->name('newsletter.subscribe');
+
+// Route::get('/buyer', [HomeController::class, 'index'])->name('buyer.index');
+// Route::get('/buyer/categories', [HomeController::class, 'categories'])->name('buyer.categories');
+// Route::get('/buyer/top-products', [HomeController::class, 'topProducts'])->name('buyer.top-products');
+// Route::get('/buyer/search-suggestions', [HomeController::class, 'searchSuggestions'])->name('buyer.search-suggestions');
